906c48fc91fdf3518fecf79e957cd618fc117b5b
levi-rs <levi.noecker@rewardstyle.com>
2017-06-25 07:17:14 -0500

    Update docstrings and help() messages

--- traw/__init__.py
+++ traw/__init__.py
@@ -1,5 +1,21 @@
+"""  TRAW: TestRail API Wrapper
+
+TRAW is an API wrapper for Gurrock's TestRail test management suite
+
+The intended way to begin is to instantiate the TRAW Client:
+
+.. code-block:: python
+    import traw
+    testrail = traw.Client(username='username',
+                           user_api_key='api_key',
+                           url='url')
+
+See the Client help documentation (`help(traw.Client)`) for more information
+"""
 from pbr.version import VersionInfo
 
-__version__ = VersionInfo('instabrade').semantic_version().release_string()
-
 from .client import Client  # NOQA
+
+
+__version__ = VersionInfo('instabrade').semantic_version().release_string()
+__all__ = ('__version__', 'Client')
